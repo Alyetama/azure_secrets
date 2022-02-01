66e57ba297a8a43b85f111db1f56d097b0d3d9d3
Alyetama <56323389+Alyetama@users.noreply.github.com>
2022-02-01 08:09:48 -0500

    update

--- azure_secrets.py
+++ azure_secrets.py
@@ -1,4 +1,5 @@
 """azure secrets"""
+import functools
 import os
 from pathlib import Path
 
@@ -7,18 +8,11 @@ from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
 
 
-class AzureSecrets:
-	"""
-	- set a secret:
-		client.set_secret(secret_name, secret_value)
-	- get a secret:
-		client.get_secret(secret_name)
-	- delete a secret:
-		az.delete_secret(secret_name)
-	"""
+class AzureSecretsClient:
 	def __init__(self, env_file='~/.zshenv'):
 		self.env_file = env_file
 
+	@functools.cache
 	def client(self):
 		load_dotenv(Path(self.env_file).expanduser())
 		keyVaultName = os.environ["KEY_VAULT_NAME"]
@@ -27,9 +21,30 @@ class AzureSecrets:
 		client = SecretClient(vault_url=KVUri, credential=credential)
 		return client
 
+
+class AzureSecrets:
+	"""
+	client = AzureSecretsClient().client()
+	az = AzureSecrets(client)
+	- set a secret:
+		az.set_secret(secret_name, secret_value)
+	- get a secret:
+		az.get_secret(secret_name)
+	- delete a secret:
+		az.delete_secret(secret_name)
+	"""
+	def __init__(self, client):
+		self.client = client
+
+	def get_secret(self, name, **kwargs):
+		return self.client.get_secret(name, **kwargs)
+
+	def set_secret(self, name, value, **kwargs):
+		return self.client.set_secret(name, value, **kwargs)
+
 	def delete_secret(self, secret_name):
-		poller = client.begin_delete_secret(secret_name)
+		poller = self.client.begin_delete_secret(secret_name)
 		deleted_secret = poller.result()
 
 
-__version__ = "0.0.5"
+__version__ = "0.0.7"
